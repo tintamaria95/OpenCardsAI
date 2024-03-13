4276bc94907f69333be818fab180948363b964f9
tintamaria95 <tintamaria95@hotmail.fr>
2024-03-13 01:47:08 +0100

    changed vite config to accept process.env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 console.log('vite config loaded...')
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      'localhost:3000': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define:{
+      'process.env': env
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        'localhost:3000': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
